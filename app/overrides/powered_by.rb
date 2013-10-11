5b41399881013e81038747f0cd41a8408bf40ee6
Anil Kumar Muppalla <anil.kumar.848@gmail.com>
2013-10-11 21:52:59 +0530

    updated the footer

--- app/overrides/powered_by.rb
+++ app/overrides/powered_by.rb
@@ -1,3 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/shared/_footer", 
                      :name => "footer-left", 
-                     :remove => "#footer-left")
+                     :replace_contents => "#footer-left") do 
+                     '<p><%= Spree.t :powered_by  %> <%= link_to "GiftSetGo!", "http://giftsetgo.com/"%></p>'
+                 end
